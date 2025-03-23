7c18d13251219c1b0c90d9605d65811b47b6d939
abdosamo1 <136762539+abdosamo1@users.noreply.github.com>
2025-03-23 11:24:45 +0100

    render pizzas with styling

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+document.addEventListener("DOMContentLoaded", function () {
+    renderRestaurantMenu();
+});
+
 function toggleNavBar() {
     
     let navbar = document.getElementById("links");
@@ -7,4 +11,23 @@ function toggleNavBar() {
 function toggleCart() {
     let cart = document.getElementById("cart-contents");
     cart.classList.toggle("visible");
+}
+
+function renderRestaurantMenu() {
+
+    let restaurantMenu = document.getElementById("restaurant-menu");
+    for (let i = 0; i < pizzas.length; i++) {
+        let pizza = pizzas[i]
+        restaurantMenu.innerHTML += `
+        <div class="Pizza-Item">
+            <div class="item-description">
+                <h2> ${pizza.name}: </h2>
+                <p> ${pizza.ingredients}</p>
+                <p> Price: ${pizza.price} €</p>
+            </div>
+                <a class="cart-icon" onclick="toggleCart()">
+                    <i class="material-icons">add_shopping_cart</i>
+                </a>  
+        </div>`;
+    }
 }
--- scripts/Pizzeria.js
+++ scripts/Pizzeria.js
@@ -1,6 +1,22 @@
-pizzeria = {
-    pizzaMargaritta: {
-        size: "medium",
-        toppings: ["cheese", "pepperoni", "mushrooms"],
+let pizzas = [
+    {
+        name: "Pizza Margaritta",
+        ingredients: "mit milder Tomatensauce und leckerem Edamer Käse",
+        price: "6,99"},
+    {   name: "Pizza Salami",
+        ingredients: "mit saftiger Salami auf milder Tomatensauce und leckerem Edamer Käse",
+        price: "7,99"
+    },
+    {   name: "Pizza Schinken",
+        ingredients: "mit saftigem Hinterschinken auf milder Tomatensauce und leckerem Edamer Käse",
+        price: "7,99"
+    },
+    {   name: "Pizza Alaska",
+        ingredients: "mit saftigem Thunfisch und roten Zwiebeln auf milder Tomatensauce und leckerem Edamer Käse",
+        price: "9,99"
+    },
+    {    name: "Pizza Suck",
+        ingredients: "mit würziger Suck auf milder Tomatensauce und leckerem Edamer Käse",
+        price: "8,99"
     }
-}
+    ]
