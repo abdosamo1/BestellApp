85709eb7d239b2091017018816a45f97e5fbfb99
abdosamo1 <136762539+abdosamo1@users.noreply.github.com>
2025-03-23 11:33:33 +0100

    template verbesserung

--- script.js
+++ script.js
@@ -18,16 +18,6 @@ function renderRestaurantMenu() {
     let restaurantMenu = document.getElementById("restaurant-menu");
     for (let i = 0; i < pizzas.length; i++) {
         let pizza = pizzas[i]
-        restaurantMenu.innerHTML += `
-        <div class="Pizza-Item">
-            <div class="item-description">
-                <h2> ${pizza.name}: </h2>
-                <p> ${pizza.ingredients}</p>
-                <p> Price: ${pizza.price} €</p>
-            </div>
-                <a class="cart-icon" onclick="toggleCart()">
-                    <i class="material-icons">add_shopping_cart</i>
-                </a>  
-        </div>`;
+        restaurantMenu.innerHTML += pizzaItem(pizza);
     }
 }
---
+++ scripts/Templates.js
@@ -0,0 +1,13 @@
+function pizzaItem(pizza) {
+return `
+        <div class="Pizza-Item">
+            <div class="item-description">
+                <h2> ${pizza.name}: </h2>
+                <p> ${pizza.ingredients}</p>
+                <p> Price: ${pizza.price} €</p>
+            </div>
+                <a class="cart-icon" onclick="toggleCart()">
+                    <i class="material-icons">add_shopping_cart</i>
+                </a>  
+        </div>`    
+}
