4175bf4f41ac4672d1d4a54c68091032058fe932
abdosamo1 <136762539+abdosamo1@users.noreply.github.com>
2025-03-25 12:21:04 +0100

    adding item to cart

--- script.js
+++ script.js
@@ -1,13 +1,21 @@
-
 let cart = document.getElementById("cart-contents");
+let emptyCart = document.getElementById("empty-cart")
+let navbar = document.getElementById("links");
+
+
+function displayPizzas() {
+    let restaurantMenu = document.getElementById("restaurant-menu");
+    for (let pizzaNumber = 0; pizzaNumber < pizzas.length; pizzaNumber++) {
+        let pizza = pizzas[pizzaNumber]
+        restaurantMenu.innerHTML += addPizza(pizza , pizzaNumber);
+    }
+}
 
 function renderRestaurantMenu() {
     displayPizzas()
 }
 
 function toggleNavBar() {
-    
-    let navbar = document.getElementById("links");
     navbar.classList.toggle("visible");
 }
 
@@ -15,13 +23,10 @@ function toggleCart() {
     cart.classList.toggle("visible");
 }
 
-function addToCart() {
+function addToCart(index) {
+    let pizza = pizzas[index];
     cart.classList.add("visible");
-    cart.innerHTML += `<div class"added-to-cart">
-                            <p></p>
-                       </div>`;
+    emptyCart.classList.add("d_none");
+    cart.innerHTML += addItemToCart(pizza);
 }
 
-document.addEventListener("DOMContentLoaded", function () {
-    renderRestaurantMenu();
-});
--- scripts/Templates.js
+++ scripts/Templates.js
@@ -1,22 +1,47 @@
-function addPizza(pizza) {
-return `
+let itemAmount = 1;
+
+function addPizza(pizza , index) {
+    return `
         <div class="Pizza-Item">
             <div class="item-description">
                 <h2> ${pizza.name}: </h2>
                 <p> ${pizza.ingredients}</p>
                 <p> Price: ${pizza.price} €</p>
             </div>
-                <a class="cart-icon" onclick="addToCart()">
+                <a class="cart-icon" onclick="addToCart(${index})">
                     <i class="material-icons">add_shopping_cart</i>
                 </a>  
         </div>`    
 }
 
-function displayPizzas() {
-    let restaurantMenu = document.getElementById("restaurant-menu");
-    for (let pizzaNumber = 0; pizzaNumber < pizzas.length; pizzaNumber++) {
-        let pizza = pizzas[pizzaNumber]
-        restaurantMenu.innerHTML += addPizza(pizza);
-    }
+
+function addItemToCart(pizza) {
+    return `<div class="added-to-cart">
+                            <p>${pizza.name}:</p>
+                            <div class="item-preis-amount">
+                                <div class="cart-icons">
+                                      ${removeOneTemplate()} ${itemAmount} ${addOneTemplate()}
+                                </div>
+                                <span>Price: ${pizza.price} €</span>
+                                ${removeItemTemplate()}
+                            </div>
+                       </div>`
 }
 
+function removeOneTemplate() {
+    return `<a onclick="">
+                    <i class="material-icons cart-icon">remove</i>
+                </a> `
+}
+
+function addOneTemplate() {
+    return `<a onclick="">
+                    <i class="material-icons cart-icon">add</i>
+                </a> `
+}
+
+function removeItemTemplate() {
+    return `<a onclick="">
+                    <i class="material-icons cart-icon">delete</i>
+                </a> `
+}
